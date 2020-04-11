304c1b4bfeb21e3b50b25c1f6604ed8366d4f94f
Danshotgames <40488455+Danshotgames@users.noreply.github.com>
2020-04-11 21:30:48 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,225 @@
+import discord
+from discord.ext import commands
+import asyncio
+import random
+
+client = discord.Client()
+
+client = commands.Bot( command_prefix = '//' )
+
+	
+
+#Variables
+
+serverid = 625001454666776586
+rainbowrolename = "АДМИН"
+delay = 1
+link = ''
+
+
+
+#Messages
+
+#Filter
+
+#@client.event
+#async def on_message (message):
+    #channel = client.get_channel( 625001455182544937 )
+
+    #await client.process_commands(message)
+
+    #msg = message.content.lower()
+    #if msg in bad_words:
+        #await message.delete()
+        #await message.author.send(f'{message.author.name}, нельзя такое писать в этом чате!')
+
+#Commands
+
+#Rainbow
+
+colours = [discord.Color.dark_orange(),discord.Color.orange(),discord.Color.dark_gold(),discord.Color.gold(),discord.Color.dark_magenta(),discord.Color.magenta(),discord.Color.red(),discord.Color.dark_red(),discord.Color.blue(),discord.Color.dark_blue(),discord.Color.teal(),discord.Color.dark_teal(),discord.Color.green(),discord.Color.dark_green(),discord.Color.purple(),discord.Color.dark_purple()]
+
+async def rainbowrole(role):
+    for role in client.get_guild(serverid).roles:
+        if str(role) == str(rainbowrolename):
+            print("detected role")
+            while not client.is_closed():
+                try:
+                    await role.edit(color=random.choice(colours))
+                except Exception:
+                    print("can't edit role, make sure the bot role is above the rainbow role and that is have the perms to edit roles")
+                    pass
+                await asyncio.sleep(delay)
+    print('role with the name "' + rainbowrolename +'" not found')
+    print("creating the role...")
+    try:
+        await client.get_guild(serverid).create_role(reason="Created rainbow role", name=rainbowrolename)
+        print("role created!")
+        await asyncio.sleep(2)
+        client.loop.create_task(rainbowrole(rainbowrolename))
+    except Exception as e:
+        print("couldn't create the role. Make sure the bot have the perms to edit roles")
+        print(e)
+        pass
+        await asyncio.sleep(10)
+        client.loop.create_task(rainbowrole(rainbowrolename))
+
+@client.event
+async def on_ready():
+    client.loop.create_task(rainbowrole(rainbowrolename))
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('Ready.')
+    print('------------')
+    print( 'bot connected' )
+
+
+#Mute
+
+@client.command()
+@commands.has_permissions(kick_members=True)
+
+async def mute (ctx, member: discord.Member):
+    await ctx.channel.purge(limit=1)
+
+    mrole = discord.utils.get(ctx.message.guild.roles, name='Muted')
+
+    emb = discord.Embed( title = 'ВНИМАНИЕ!',colour = discord.Color.purple(),url = None )
+
+    emb.set_author( name = ctx.author.name, icon_url = ctx.author.avatar_url )
+    
+    emb.add_field( name ='----------------', value = 'У {} ограничения чата за нарушение прав!'.format(member.mention) )
+
+    await ctx.send(embed = emb)
+    await member.add_roles( mrole )
+
+#Unmute
+@client.command()
+@commands.has_permissions(kick_members=True)
+
+async def unmute(ctx, member: discord.Member):
+    await ctx.channel.purge(limit=1) 
+
+    drole = discord.utils.get(ctx.message.guild.roles, name = 'Muted')
+    
+    #await ctx.send(f"{} снял ограничения чата у{member.mention}")
+    emb = discord.Embed( title = 'ВНИМАНИЕ!',colour = discord.Color.purple(),url = None )
+
+    emb.set_author( name = ctx.author.name, icon_url = ctx.author.avatar_url )
+    
+    emb.add_field( name ='----------------', value = 'У {} сняли ограничения чата!'.format(member.mention) )
+
+    await ctx.send(embed = emb)
+    await member.remove_roles(drole)
+#Join
+@client.command()
+@client.event
+
+async def on_member_join( member ):
+    channel = client.get_channel( 698183270466060349 )
+    await member.send(f'Поздравляем {member.mention}! теперь Вы часть нашего клана! Добро пожаловать на сервер! ')
+    role = discord.utils.get( member.guild.roles, id = 698157175561912543 )
+
+    await member.add_roles( role )
+    #emb = discord.Embed( title = 'НОВЫЙ УЧАСТНИК!',colour = discord.Color.purple(),url = None )
+    await channel.send( embed = discord.Embed( description = f'Пользователь { member.mention }, присоединился к нам! Добро пожаловать!', colour = discord.Color.purple() ) )
+
+#links
+@client.command()
+@client.event
+async def new( ctx):
+    
+    await ctx.channel.purge (limit = 1)
+    
+    emb = discord.Embed( title = 'РАССЫЛКА!', description = f'У Ская вышло новое видео!!! Бегом смотреть! Ссылка: {link}',colour = discord.Color.purple(),url = None )
+    await ctx.send(embed = emb)
+
+#Clear
+    
+@client.command( pass_context = True )
+@commands.has_permissions( kick_members = True )
+
+async def clear( ctx, amount = 100 ):
+    await ctx.channel.purge(limit = amount+1)
+
+    emb = discord.Embed( title = 'УДАЛЕНИЕ', description = f'Удалено: {amount} сообщений',colour = discord.Color.purple(),url = None )
+    await ctx.send(embed = emb)
+    
+#Help
+    
+@client.command( pass_context = True )
+
+async def info( ctx ):
+    await ctx.channel.purge (limit = 1)
+
+    emb = discord.Embed( title = 'ИНФОРМАЦИЯ', description = 'ГИД по коммандам:',colour = discord.Color.purple(),url = None )
+
+    emb.add_field( name ='//info', value = 'Инструкция по коммандам' )
+    emb.add_field( name ='//clear', value = 'Очистка чата')
+    emb.add_field( name ='//kick', value = 'Выгнать участника')
+    emb.add_field( name ='//ban', value = 'Заблокировать участника')
+    emb.add_field( name ='//mute', value = 'Ограничить чат участника')
+    emb.add_field( name ='//unmute', value = 'Убрать ограничения чата участника')
+    await ctx.send(embed = emb)
+    
+#Kick
+
+@client.command( pass_context = True )
+@commands.has_permissions( kick_members = True )
+
+async def kick( ctx, member: discord.Member, *, reason = None):
+
+    await ctx.channel.purge (limit = 1)
+    emb = discord.Embed( title = 'ВНИМАНИЕ!',colour = discord.Color.purple(),url = None )
+
+    emb.set_author( name = ctx.author.name, icon_url = ctx.author.avatar_url )
+    await member.send(f'{member.mention}, Вы были выгнаны из сервера! Причина: {reason}')
+    #emb.set_thumbnail( url = '' )
+    emb.add_field( name ='Kicked', value = 'ВЫГНАН УЧАСТНИК: {}'.format(member.mention) )
+    emb.add_field( name ='Reason', value = f'ПРИЧИНА: {reason} ' )
+    await ctx.send(embed = emb)
+    #await message.author.send(f'{message.author.name}, вы были выгнаны из сервера! Причина: {reason}')
+    await member.kick( reason = reason )
+    #await ctx.send( f'АДМИНОМ ОТПИЗЖЕН {member.mention}')
+    
+ 
+#Ban
+
+@client.command( pass_context = True )
+@commands.has_permissions( ban_members = True )
+
+async def ban( ctx, member: discord.Member, *, reason = None):
+    await ctx.channel.purge (limit = 1)
+    emb = discord.Embed( title = 'ВНИМАНИЕ!',colour = discord.Color.purple(),url = None )
+    await member.send(f'{member.mention}, Вы были заблокированы на сервере! Причина: {reason}')
+    emb.set_author( name = ctx.author.name, icon_url = ctx.author.avatar_url )
+    #emb.set_footer( text = ctx.author.name, icon_url = ctx.author.avatar_url )
+    #emb.set_thumbnail( url = '' )
+    emb.add_field( name ='Banned', value = 'ЗАБАНЕН УЧАСТНИК: {}'.format(member.mention) )
+    emb.add_field( name ='Reason', value = f'ПРИЧИНА: {reason} ' )
+    await ctx.send(embed = emb)
+    await member.ban( reason = reason )
+     #await ctx.send( f'АДМИНОМ ВЫЕБАН {member.mention}')
+
+
+#Embed
+
+@client.command( pass_context = True )
+@commands.has_permissions( ban_members = True )
+
+async def test( ctx ):
+    emb = discord.Embed( title = 'Тестовая хуйня', description = 'КАКАЯ-ТО ПОЕБОТА ОТ АДМИНА',colour = discord.Color.purple(),url = None )
+
+    #emb.set_author( name =  client.user.name, icon_url = client.user.avatar_url )
+    #emb.set_footer( text = ctx.author.name, icon_url = ctx.author.avatar_url )
+    #emb.set_thumbnail( url = '' )
+
+    await ctx.send(embed = emb)
+
+
+#Connect
+
+token = open( 'token.txt', 'r' ).readline()
+
+client.run( token )    
